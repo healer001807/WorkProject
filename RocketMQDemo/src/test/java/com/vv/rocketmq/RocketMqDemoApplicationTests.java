e97a2d63d6405dd979473f2cf8729de265b8e006
helear <helear@666.com>
2023-10-21 22:15:57 +0800

    rocket mq 多生产者遗漏文件

---
+++ RocketMQDemo/src/test/java/com/vv/rocketmq/RocketMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vv.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
